6b4fda9cdea5e74f3544aa3c6fe9fb855288e61f
gohlhausen <gaylano@gmail.com>
2020-02-24 00:48:20 +0000

    Changes to be committed: 	modified:   FFTPanel 	modified:   FFTPanel.cc 	modified:   FFTPanel.o 	modified:   graphFFTS16_LEMatrix 	modified:   graphFFTS16_LEMatrix.o

--- recordb/FFTPanel.cc
+++ recordb/FFTPanel.cc
@@ -482,7 +482,7 @@ int main (int argc, char *argv[])
   //short * sampleRP;
   int err;
   char *buffer[buffers];
-  const char* arg="plughw:1,0";
+  const char* arg="hw:CARD=Device,DEV=0";
   unsigned int rate = 48000;
   snd_pcm_t *capture_handle;
   snd_pcm_hw_params_t *hw_params;
